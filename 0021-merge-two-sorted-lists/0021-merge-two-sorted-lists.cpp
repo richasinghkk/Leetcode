d8ac689fee93d6d8d3ad8dce883f356baec381c5
Richa Singh <146985975+richasinghkk@users.noreply.github.com>
2024-10-23 21:51:10 +0530

    Time: 0 ms (100%), Space: 19.8 MB (55.98%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,57 +1,11 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode*a, ListNode* b) {
-        // ListNode*tempA=a;
-        // ListNode*tempB=b;
-        // ListNode*c=new ListNode(100);
-        // ListNode*tempC=c;
-        // while(tempA!=NULL && tempB!=NULL){
-        //     if(tempA->val<=tempB->val){
-        //         ListNode*t= new ListNode(tempA->val);
-        //         tempC->next=t;
-        //         tempC=t;
-        //         tempA=tempA->next;
-        //     }
-        //     else{
-        //         ListNode*t=new ListNode(tempB->val);
-        //         tempC->next=t;
-        //         tempC=t;
-        //         tempB=tempB->next;
-        //     }
-        // }
-        // if(tempA==NULL){
-        //     tempC->next=tempB;
-        // }
-        // else{
-        //     tempC->next=tempA;
-        // }
-        // return tempC->next;
-        ListNode*c=new ListNode(100);
-        ListNode*temp=c;
-    while(a!=NULL && b!=NULL){
-        if(a->val<=b->val){
-            temp->next=a;
-            a=a->next;
-            temp=temp->next;
-        }
-        else{
-            temp->next=b;
-            b=b->next;
-            temp=temp->next;
-        }
-    }
-    if(a==NULL) temp->next=b;
-    else temp->next=a;
-    return c->next;
-    }
+ public:
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    if (!list1 || !list2)
+      return list1 ? list1 : list2;
+    if (list1->val > list2->val)
+      swap(list1, list2);
+    list1->next = mergeTwoLists(list1->next, list2);
+    return list1;
+  }
 };
